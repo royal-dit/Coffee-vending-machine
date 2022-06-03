5608ea94738cd5e11f8d040da3ac5164f61401f5
Royal dahal <58435678+royal-dit@users.noreply.github.com>
2022-06-03 13:32:39 +0545

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+
+
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+is_on = True
+menu = Menu()
+
+#print Report
+coffee_maker.report()
+money_machine.report()
+
+#check Sufficent Resources
+
+while is_on:
+    options = menu.get_items()
+    choice = input(f"What would you Like?({options}): ")
+    if choice == 'off':
+        is_on = False
+    elif choice == 'report':
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(drink):
+            if money_machine.make_payment(drink.cost):
+                coffee_maker.make_coffee(drink)
